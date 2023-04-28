568e34c52c389e797ba98093a73defff18e031ca
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-04-28 12:32:17 +0530

    greeting msg

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LinqDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Product Review Management Problem ");
+        }
+
+    }
+        
+}
