f83bec214fe19f6d15f801bccb5b65aabae8a758
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-04-28 16:15:33 +0530

    retrive records from productid

--- Managemenent.cs
+++ Managemenent.cs
@@ -9,15 +9,14 @@ namespace LinqDemo
 {
     public class Management
     {
-        public void SelectRecords(List<ProductReview> ListOfProductReview)
-         {
-            var RecordData = from Products in ListOfProductReview
-                             where (Products.ProductID == 1 || Products.ProductID == 7 || Products.ProductID == 9)
-                             && Products.Rating > 3
-                             select Products;
+        public void RetiveCountOfRecords(List<ProductReview> ListOfProductReview)
+        {
+            var RecordData = ListOfProductReview.
+                            GroupBy(x => x.ProductID).
+                             Select(x => new { ProductID = x.Key, count = x.Count() });
             foreach (var Record in RecordData)
             {
-                Console.WriteLine("{0} {1} {2} {3} {4}", Record.ProductID, Record.UserID, Record.Rating, Record.Review, Record.IsLike);
+                Console.WriteLine("For ProductID = {0} the records are {1}", Record.ProductID, Record.count);
             }
 
         }
