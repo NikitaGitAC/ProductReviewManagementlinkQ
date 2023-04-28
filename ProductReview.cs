609f5400fd0107f56e3ac01a08534650985a117f
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-04-28 13:26:28 +0530

    create model for list

--- ProductReview.cs
+++ ProductReview.cs
@@ -8,11 +8,15 @@ namespace LinqDemo
 {
     public class ProductReview
     {
-
         public int ProductID { get; set; }
+
         public int UserID { get; set; }
+
         public double Rating { get; set; }
+
         public string Review { get; set; }
+
         public bool IsLike { get; set; }
+
     }
 }
